a3b8e90a6f561dad5565e0f9e15cfd48d6667743
boer <boer0924@qq.com>
2016-08-07 22:03:30 +0800

    add remote exec

--- analyze_nginx_log.py
+++ analyze_nginx_log.py
@@ -1,8 +1,38 @@
-#!/usr/bin/python
+#!venv/bin/python
 # coding=utf-8
 import json
-from collections import Counter # Python2.7 feature
-# import re
+from collections import Counter  # Python2.7 feature
+import os
+import paramiko as ssh
+
+# 'sudo apt install libssl-dev libffi-dev python-dev'
+
+client = ssh.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(ssh.AutoAddPolicy())
+client.connect('127.0.0.1', 22, 'boer', os.environ.get('SSH_PASSWD'))
+# stdin, stdout, stderr = client.exec_command('ls -l /home/boer')
+# export SSH_PASSWD = '123456'
+
+t = client.get_transport()
+channel = t.open_session()
+channel.exec_command('sudo cp /var/log/nginx/access.log /tmp/access.log && sudo chown app. /tmp/access.log')
+exit_code = channel.recv_exit_status()
+if exit_code == 0:
+    ftp = client.open_sftp()
+    remotepath = '/tmp/access.log'
+    localpath = '/tmp/access.log-' + host
+    ftp.get(remotepath, localpath)
+    ftp.close()
+    t.close()
+    client.close()
+else:
+    print 'remote command error'
+    t.close()
+    client.close()
+    sys.exit()
+
+
 count = []
 counter = {}
 with open('./access.log', 'r') as f:
@@ -10,7 +40,8 @@ with open('./access.log', 'r') as f:
         lines.replace('\\x', '\\\\x')
         results = json.loads(lines)
         if results['http_x_forwarded_for'].decode('utf-8').find(',') != -1:
-            if results['http_x_forwarded_for'].startswith(('192.168', '10.', '172.16')):
+            if results['http_x_forwarded_for'].startswith(
+                ('192.168', '10.', '172.16')):
                 custom_ip = results['http_x_forwarded_for'].split(',')[1]
             else:
                 custom_ip = results['http_x_forwarded_for'].split(',')[0]
