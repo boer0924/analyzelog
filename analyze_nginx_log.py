6e683c2b2df0739e9570e329eea5d9e824432335
boer <boer0924@gmail.com>
2016-08-26 16:49:59 +0800

    ok

--- analyze_nginx_log.py
+++ analyze_nginx_log.py
@@ -1,55 +1,55 @@
 #!venv/bin/python
-# coding=utf-8
+# -*- coding: utf-8 -*-
+
+__author__ = 'boer'
+
 import json
 import re
 import os
 import sys
-import time
-# import paramiko as ssh
-from multiprocessing import Pool
+from multiprocessing import Process, Pool
+import paramiko as ssh
 
 # 'sudo apt install libssl-dev libffi-dev python-dev'
 
+def red_text(txt):
+    return '\033[31m' + txt + '\033[0m'
 
-# def get_log(host):
-#     client = ssh.SSHClient()
-#     client.load_system_host_keys()
-#     client.set_missing_host_key_policy(ssh.AutoAddPolicy())
-#     client.connect(host, 22, 'boer', os.environ.get('SSH_PASSWD'))
-#     stdin, stdout, stderr = client.exec_command('')
-#     # export SSH_PASSWD = '123456'
+def green_text(txt):
+    return '\033[32m' + txt + '\033[0m'
 
-#     t = client.get_transport()
-#     channel = t.open_session()
-#     channel.exec_command(
-#         'sudo cp /var/log/nginx/access.log /tmp/access.log && sudo chown app. /tmp/access.log')
-#     exit_code = channel.recv_exit_status()
-#     if exit_code == 0:
-#         ftp = client.open_sftp()
-#         remotepath = '/tmp/access.log'
-#         localpath = '/tmp/access.log-' + host
-#         ftp.get(remotepath, localpath)
-#         ftp.close()
-#         t.close()
-#         client.close()
-#     else:
-#         print 'remote command error'
-#         t.close()
-#         client.close()
-#         sys.exit()
-#     return
+def get_log(host):
+    'ftp web服务器nginx日志.'
+    client = ssh.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(ssh.AutoAddPolicy())
+    client.connect(host, 22, 'app')
+
+    t = client.get_transport()
+    channel = t.open_session()
+    channel.get_pty()
+    channel.exec_command('sudo cp -f /var/log/nginx/access.log /tmp/access.log && sudo chown app. /tmp/access.log')
+    print '...', channel.recv(9999)
+    print '...', channel.recv_stderr(9999)
+
+    ftp = client.open_sftp()
+    remotepath = '/tmp/access.log'
+    localpath = '/tmp/access.log-' + host
+    ftp.get(remotepath, localpath)
+    ftp.close()
+
+    t.close()
+    client.close()
 
 
 def analyze_log(logfile):
-    start = time.time()
-    print 'Current log %s, -> pid [%s]' % (logfile, os.getpid())
-    # count = []
+    'analyze单台服务器日志.'
     counter = {}
     with open(logfile, 'r') as f:
         for lines in f:
-            # lines = re.sub(r'\\', r'\\\\', lines)
-            # lines = lines.replace('\\', '\\\\')
+            lines = re.sub(r'\\', r'\\\\', lines)
             results = json.loads(lines)
+            # real client IP.
             if results['http_x_forwarded_for'].decode('utf-8').find(',') != -1:
                 if results['http_x_forwarded_for'].startswith(
                     ('192.168', '10.', '172.16')):
@@ -59,45 +59,68 @@ def analyze_log(logfile):
             else:
                 if results['http_x_forwarded_for'] != '-':
                     custom_ip = results['http_x_forwarded_for']
-            # count.append(custom_ip)
+            # bad request or response log.
+            if results['status'].decode('utf-8') in ['400']:
+                with open('/tmp/bad_log.log', 'a') as log:
+                    log.write(lines)
+            # counter IP
             if custom_ip in counter:
                 counter[custom_ip] += 1
             else:
                 counter[custom_ip] = 1
-    end = time.time()
-    print '共用时：', (end - start)
-    print '=' * 50
+
     return counter
 
 
-def exec_analyze(logfiles):
-    results = []
-    p = Pool(len(logfiles))
-    for logfile in logfiles:
-        results.append(p.apply_async(analyze_log, args=(logfile,)).get())
+def exec_get_log(hosts):
+    'multiprocessing 获取多台服务器日志.'
+    p = Pool(len(hosts))
+    for host in hosts:
+        p.apply_async(get_log, args=(host,))
+        print red_text('Get log file done.by'), green_text(host)
     p.close()
     p.join()
-    return results
+    print green_text('Get all log done...')
+
+
+#def exec_analyze(logfiles):
+#    p = Pool(len(logfiles))
+#    results = []
+#    for logfile in logfiles:
+#        results.append(p.apply_async(analyze_log, args=(logfile,)).get())
+#        print red_text(logfile), green_text('analyze done...')
+#    p.close()
+#    p.join()
+#    return results
+
 
 if __name__ == '__main__':
+    # prod web server
     hosts = ['192.168.28.12', '192.168.28.13']
-    logfiles = ['./access.log-' + host for host in hosts]
+    # testing web server
+    # hosts = ['172.19.3.24', '172.19.3.25']
+    logfiles = ['/tmp/access.log-' + host for host in hosts]
+
+    # 调用多进程获取日志函数
+    exec_get_log(hosts)
+    
     counter = {}
-
-    print 'Start results'.center(60, '=')
-
-    for item in exec_analyze(logfiles):
+    # multiprocessing Pool对象的map函数用法.
+    p = Pool(len(logfiles))
+    results = p.map(analyze_log, logfiles)
+    for item in results:
         for k, v in item.items():
             if k in counter:
                 counter[k] += v
             else:
                 counter[k] = v
 
+    print 'Start results'.center(60, '*')
     flag = 0
+    # 根据dict值排序输出
     for item in sorted(counter.items(), key=lambda d: d[1], reverse=True):
-        print item
+        print red_text(item[0]), '=>', green_text(str(item[1]))
         flag += 1
-        if flag == 5:
+        if flag == 20:
             break
-
-    print 'End results'.center(60, '=')
+    print 'End results'.center(60, '*')
